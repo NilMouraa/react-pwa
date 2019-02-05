1368cf5d2a54baf42e83c0ff541d92dc50cc8f82
Nil Moura <nil.moura@bs2.com>
2019-02-04 23:02:28 -0200

    Updates

---
+++ precache-manifest.734cf399932e59f71feba284bd169d80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pwa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf64366b40f2be799923",
+    "url": "/react-pwa/static/js/runtime~main.bf64366b.js"
+  },
+  {
+    "revision": "1258d82ecca46f5108c5",
+    "url": "/react-pwa/static/js/main.1258d82e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-pwa/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1258d82ecca46f5108c5",
+    "url": "/react-pwa/static/css/main.dc6f6298.chunk.css"
+  },
+  {
+    "revision": "4d87a1b41544ac87a7fa3f5bd8d2a0cc",
+    "url": "/react-pwa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bb607215f50b99bfe312cf4b40c3ecb.js"
+  "/react-pwa/precache-manifest.734cf399932e59f71feba284bd169d80.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-pwa/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
